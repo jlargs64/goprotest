9479aa63c512e6c9fcf4ab7d88f4f7bd51f77fd6
Justin Largo <justinlargo7@gmail.com>
2020-07-15 17:55:56 -0400

    Can parse login and sign up post body.

--- routes/index.js
+++ routes/index.js
@@ -10,8 +10,24 @@ router.get('/login', function(req,res,next){
   res.render('login');
 });
 
+router.post('/login', function(req,res,next){
+  var email = req.body.email;
+  var password = req.body.password;
+  console.log(req.body);
+});
+
 router.get('/signup', function(req,res,next){
   res.render('signup');
+});
+
+router.post('/signup', function(req,res,next){
+  var email = req.body.email;
+  var password = req.body.password;
+  var password2 = req.body.password2;
+  
+  // Check password and password2 if they match
+
+  console.log(req.body);
 });
 
 module.exports = router;
